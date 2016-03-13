32137d10db8dc322fc532ea799992e24a9bb2acd
ABHISHEK GSWAMI <abhishekg785@gmail.com>
2016-03-14 00:23:07 +0530

    api for delete and update created

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def not_found(error):
     return make_response(jsonify({'error':'Not Found'}),404)
 
 #api for deleting a particular task
-@app.route('')
+@app.route('/todo/api/v1.0/tasks/<int:task_id>',methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id']==task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result':True})
 
 
 #api for posting the data
@@ -54,6 +60,25 @@ def get_task(task_id):
 @app.route('/todo/api/v1.0/tasks',methods = ['GET'])
 def get_tasks():
     return jsonify({'tasks':tasks})
+
+#api for updating the data
+@app.route('/todo/api/v1.0/tasks/<int:task_id>',methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) != unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) != bool:
+        abort(400)
+    task[id]['title'] = request.json.get('title')
+    task[id]['description'] = request.json.get('description')
+    task[id]['done'] = request.json.get('done')
+    return jsonify({'task':task[0]})
 
 if(__name__ == '__main__'):
     app.run(debug=True)
